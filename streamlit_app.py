a692383d2039d6a440e812fbf55b284d18a6a6ed
shettyarchana098 <107310700+shettyarchana098@users.noreply.github.com>
2022-06-13 09:54:37 +0530

    Update streamlit_app.py
    import pandas to streamlit
    After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,9 @@ streamlit.text('🥑🍞 Avacado toast')
 
 #add header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#PANDAS-python package library
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing:
+streamlit.dataframe(my_fruit_list)
