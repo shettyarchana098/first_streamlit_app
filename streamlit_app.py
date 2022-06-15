da51e3f5095b0a631409ae833009b83d7b769176
shettyarchana098 <107310700+shettyarchana098@users.noreply.github.com>
2022-06-15 16:12:32 +0530

    #Query some data from snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,10 @@ import snowflake.connector
 #Let's Query Our Trial Account Metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select current_user(), current_account(),current_region()")
+#my_cur.execute("select current_user(), current_account(),current_region()")
+my_cur.execute("select * from fruit_load_list") #Query some data from snowflake table
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from snowflake")
+#streamlit.text("Hello from snowflake")
+streamlit.text("Fruit list load contains")
 streamlit.text(my_data_row )
 
