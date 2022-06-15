4d999e1132a038dae69928b05929a75e137c4f9d
shettyarchana098 <107310700+shettyarchana098@users.noreply.github.com>
2022-06-15 16:08:33 +0530

    #Let's Query Our Trial Account Metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,11 @@ streamlit.dataframe(fruityvice_normalized)
 #import snowflake connector
 import snowflake.connector
 
+#Let's Query Our Trial Account Metadata
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(),current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from snowflake")
+streamlit.text(my_data_row )
+
